6fab7d67179d241576d6474c0d3001bcd8466413
Darryn Mitussis <darryn@MacBook-Pro.local>
2017-01-08 23:18:19 +0200

    Academic advisor conversations

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -75,7 +75,7 @@ exports.handle = function (client) {
 			};
 
 
-			client.addResponse('provide_advisor', tutorData);
+			client.addResponse('app:response:name:provide_advisor', tutorData);
 			client.done();
 		},
 	});
@@ -100,7 +100,7 @@ exports.handle = function (client) {
 				role: "testrole",
 			};
 
-			client.addResponse('provide_advisor', tutorData);
+			client.addResponse('app:response:name:provide_advisor', tutorData);
 			client.done();
 		},
 	});
